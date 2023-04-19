ebd9384ed7a79b025c44bf6f3ee7e495f18bc2b4
shagun <shagun08081999@gmail.com>
2023-04-20 00:01:36 +0530

    Login/Register page css 👍

--- frontEnd/src/pages/login/Login.jsx
+++ frontEnd/src/pages/login/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import {Link} from "react-router-dom";
+import "./style.scss"
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="loginBox">
+    <span className="borderLine"></span>
+      <form>
+        <h2>Login</h2>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>User name</span>
+          <i></i>
+        </div>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <div className="links">
+          <a href="">Forgot Password</a>
+          <Link to="/register">Register</Link>
+        </div>
+        <input type="submit" value="Login" ></input>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- frontEnd/src/pages/register/Register.jsx
+++ frontEnd/src/pages/register/Register.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="box">
+      <span className="borderLine"></span>
+      <form>
+        <h2>Register</h2>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>User name</span>
+          <i></i>
+        </div>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>Email</span>
+          <i></i>
+        </div>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <div className="inputbox">
+          <input type="text" name="" id="" required />
+          <span>Confirm Password</span>
+          <i></i>
+        </div>
+        <div className="links">
+          <a href=""> </a>
+          <div className="allready">
+            <h5 style={{color:"#EDE9D5"}}>
+              Already have a account ?  <Link to="/login"> <h6 style={{color:"#000000",display:"inline",fontSize:"16px"}}>Login</h6></Link>
+            </h5>
+          </div>
+        </div>
+        <input type="submit" value="Register"></input>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
